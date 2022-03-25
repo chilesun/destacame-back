c935565208808e570bddd01f9818e6ecbd42be00
Daniel Flores <daniel.flores.ramirez@gmail.com>
2022-03-25 01:24:03 -0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -40,16 +40,3 @@ class SeatViewSet(viewsets.ModelViewSet):
 class TripViewSet(viewsets.ModelViewSet):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
-
-    def get_queryset(self):
-        gt = self.request.GET.get('capacity__gt')
-        if gt:
-            comply = list()
-            seats = Seat.objects.exclude(passenger=None).values('trip').order_by('trip').annotate(count=Count('trip'))
-            for seat in seats:
-                if seat['count'] * 10 >= float(gt):
-                    comply.append(seat['trip'])
-            return self.queryset.filter(id__in=comply)
-        return self.queryset
-
-
